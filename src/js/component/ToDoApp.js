d66e610a41aba51a1fffa8372c48dcc8e16c5fe5
ZeqeB <85196913+ezebellino@users.noreply.github.com>
2024-09-24 00:50:01 +0000

    realizando Todo APP

---
+++ src/js/component/ToDoApp.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import ToDoInput from "./ToDoInput";
+import ToDoList from "./ToDoList";
+
+function ToDoApp() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    if (task.trim() !== "") {
+      setTasks([...tasks, task]);
+    }
+  };
+
+  const deleteTask = (indexToDelete) => {
+    setTasks(tasks.filter((_, index) => index !== indexToDelete));
+  };
+
+  return (
+    <div className="todo-app">
+      <h2 className="text-center">ToDo APP</h2>
+      <ToDoInput addTask={addTask} />
+      {tasks.length === 0 ? (
+        <p>No hay tareas, añadir tareas</p>
+      ) : (
+        <ToDoList tasks={tasks} deleteTask={deleteTask} />
+      )}
+    </div>
+  );
+}
+
+export default ToDoApp;
---
+++ src/js/component/ToDoInput.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function ToDoInput({ addTask }) {
+  const [newTask, setNewTask] = useState("");
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      addTask(newTask);
+      setNewTask("");
+    }
+  };
+
+  return (
+    <input
+      type="text"
+      placeholder="Añadir nueva tarea"
+      value={newTask}
+      onChange={(e) => setNewTask(e.target.value)}
+      onKeyPress={handleKeyPress}
+    />
+  );
+}
+
+export default ToDoInput;
